bcde4de9768cdfa04f5da94e05fed24c186f9737
firas hamila <firas.hm.1996@gmail.com>
2025-02-15 11:33:30 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,14 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = ({ title, price }) => {
+const ExpenseItem = ({ title, price, date }) => {
+  const year = date.getFullYear();
   return (
     <div className="expense-item">
-      <div className="expense-date">date</div>
+      <div className="expense-date">
+        <div className="expense-date__month">month</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">day</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {price}</div>
