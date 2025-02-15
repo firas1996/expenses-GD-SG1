8e3bc691f9aeeabe54bdfb3bcc0cd4714b440f3e
firas hamila <firas.hm.1996@gmail.com>
2025-02-15 10:38:19 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expensesData = [
+    {
+      id: 1,
+      title: "New TV",
+      price: 799.99,
+      date: new Date("2025-02-12"),
+    },
+    {
+      id: 2,
+      title: "Voyage",
+      price: 2799.99,
+      date: new Date("2023-10-19"),
+    },
+    {
+      id: 3,
+      title: "Education",
+      price: 5000,
+      date: new Date("2024-09-20"),
+    },
+    {
+      id: 4,
+      title: "Phone",
+      price: 1500,
+      date: new Date("2025-02-7"),
+    },
+  ];
   return (
     <>
       <ExpenseItem />
