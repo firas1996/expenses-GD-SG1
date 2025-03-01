32ff85b4ee039a66a5acec20e489f7f862826739
firas hamila <firas.hm.1996@gmail.com>
2025-03-01 08:52:18 +0100

    a

--- src/components/ExpensesContainer.jsx
+++ src/components/ExpensesContainer.jsx
@@ -1,10 +1,12 @@
 import "./ExpensesContainer.css";
 import ExpenseItem from "./ExpenseItem";
 import Chart from "./Chart";
+import Filter from "./Filter";
 
 const ExpensesContainer = ({ expensesData }) => {
   return (
     <div className="expenses">
+      <Filter />
       <Chart expensesData={expensesData} />
       {expensesData.map((expense) => {
         return (
