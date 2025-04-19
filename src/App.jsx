c68e0c11535df95d575b2db5fb0b3d274c303543
firas hamila <firas.hm.1996@gmail.com>
2025-04-19 10:11:53 +0200

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
+import { useState } from "react";
 import AddExpense from "./components/AddExpense";
 import ExpensesContainer from "./components/ExpensesContainer";
 
+const expensesData = [
+  {
+    id: 1,
+    title: "New TV",
+    price: 799.99,
+    date: new Date("2025-02-12"),
+  },
+  {
+    id: 2,
+    title: "Voyage",
+    price: 2799.99,
+    date: new Date("2023-10-19"),
+  },
+  {
+    id: 3,
+    title: "Education",
+    price: 5000,
+    date: new Date("2024-08-20"),
+  },
+  {
+    id: 4,
+    title: "Phone",
+    price: 1500,
+    date: new Date("2025-02-7"),
+  },
+];
 function App() {
-  const expensesData = [
-    {
-      id: 1,
-      title: "New TV",
-      price: 799.99,
-      date: new Date("2025-02-12"),
-    },
-    {
-      id: 2,
-      title: "Voyage",
-      price: 2799.99,
-      date: new Date("2023-10-19"),
-    },
-    {
-      id: 3,
-      title: "Education",
-      price: 5000,
-      date: new Date("2024-08-20"),
-    },
-    {
-      id: 4,
-      title: "Phone",
-      price: 1500,
-      date: new Date("2025-02-7"),
-    },
-  ];
+  const [data, setData] = useState(expensesData);
   const getData = (newData) => {
-    console.log(newData);
+    setData([newData, ...data]);
   };
   return (
     <>
       <AddExpense getData={getData} />
-      <ExpensesContainer expensesData={expensesData} />
+      <ExpensesContainer expensesData={data} />
     </>
   );
 }
